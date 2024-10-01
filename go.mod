46058f29b1463a8830dbbea4fa836bfb65629d38
NathanQSilva <nathanq_@hotmail.com>
2024-10-01 09:14:56 -0300

    obtendo infos da url

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NathanQSilva/quicknotes
+
+go 1.22.1
--- main.go
+++ main.go
@@ -5,17 +5,37 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Handler")
+func noteList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Header()["Date"] = nil // Suprimir header default
+
+	fmt.Println(w, `{"id": 1}`)
+}
+
+func noteView(w http.ResponseWriter, r *http.Request) {
+	fmt.
+		fmt.Println(w, "Visualização de nota")
+}
+
+func noteCreate(w http.ResponseWriter, r *http.Request) {
+	// Rejeitar requisição
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Println(w, "Criação de nota")
 }
 
 func main() {
 	fmt.Println("Servidor rodando na porta 5000")
-
 	mux := http.NewServeMux()
-	// Rota coring / ou /hello/
-	// Rota estática /hello
-	mux.HandleFunc("/", homeHandler)
+
+	mux.HandleFunc("/", noteList)
+	mux.HandleFunc("/note/view", noteView)
+	mux.HandleFunc("/note/create", noteCreate)
 
 	http.ListenAndServe(":5000", mux)
 }
