2ec79e2985c2198a2f6ac2a2ffb99098820d9e8e
NathanQSilva <nathanq_@hotmail.com>
2024-09-30 23:28:16 -0300

    multi handlers

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"net/http"
 )
 
-type MyHandler struct{}
-
-func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world"))
+type WorldHandler struct{}
+func (WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("World"))
 }
+
+type HelloHandler struct {}
+func (HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello"))
+}
+
 
 func main() {
 	fmt.Println("Servidor rodando na porta 5000")
 
-	handler := MyHandler{}
+	world := WorldHandler{}
+	hello := HelloHandler{}
 
-	http.ListenAndServe(":5000", handler)
+	http.Handle("/hello", hello)
+	http.Handle("/world", world)
+
+	http.ListenAndServe(":5000", nil)
 }
